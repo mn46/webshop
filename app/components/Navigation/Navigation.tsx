5a96710ebb5308d19626d60ac5e84d919ac52d24
marta <m.nowak033@gmail.com>
2025-07-22 20:48:44 +0200

    Fix navigation bar to the top

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -28,7 +28,7 @@ const Navigation: React.FC = () => {
   };
 
   return (
-    <nav>
+    <nav className="fixed left-0 top-0" style={{ width: "100vw" }}>
       <div className="px-2 flex justify-between items-center">
         <Link to="/">
           <img src={Logo} />
