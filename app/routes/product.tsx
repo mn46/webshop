2a096800752ee99f45f5890aa500f63416d5219b
marta <m.nowak033@gmail.com>
2025-08-04 10:47:08 +0200

    Fetch single product data

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,8 +1,17 @@
+import { useLoaderData } from "react-router";
 import type { Route } from "./+types/product";
+import type { Product } from "~/types";
 
-const product = ({ params }: Route.ComponentProps) => {
-  console.log("params", params);
-  return <div>product</div>;
+export async function loader({ params }: Route.ClientLoaderArgs) {
+  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+  const product: Product = await res.json();
+  return product;
+}
+
+const product = () => {
+  const product: Product = useLoaderData();
+  console.log("product", product);
+  return <div>{product ? "loaded" : "LOADING..."}</div>;
 };
 
 export default product;
