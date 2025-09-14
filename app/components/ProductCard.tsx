d2e68d76092218d62ccfae3df51f2c1b1f905dac
marta <m.nowak033@gmail.com>
2025-09-14 16:03:45 +0200

    Increase the height of the title card part to even the containers

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -14,7 +14,7 @@ const ProductCard: React.FC<Props> = ({ product }) => {
       <div className="h-96 flex items-center justify-center bg-white">
         <img src={product.image} className="h-80 w-auto" />
       </div>
-      <div className="h-24 grid grid-cols-[75%_25%] md:grid-cols-[80%_20%]">
+      <div className="h-28 grid grid-cols-[75%_25%] md:grid-cols-[80%_20%]">
         <p className="bg-gray-800 text-white p-2">{product.title}</p>
         <p className=" text-white bg-green-800 p-2 text-center">
           ${product.price}
