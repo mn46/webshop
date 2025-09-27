451358858538eddb2cb92da0012cdd0628c224b8
marta <m.nowak033@gmail.com>
2025-09-27 13:35:01 +0200

    Remove unused state, hook and function

--- app/components/search/Search.tsx
+++ app/components/search/Search.tsx
@@ -1,22 +1,13 @@
-import { useState, type SetStateAction } from "react";
+import { type SetStateAction } from "react";
 import ClickOutsideWrapper from "../wrappers/ClickOutsideWrapper";
 import Button from "../Button";
-import { Form, Link, useNavigate } from "react-router";
+import { Form } from "react-router";
 
 interface Props {
   setIsSearchOpen: React.Dispatch<SetStateAction<boolean>>;
 }
 
 const Search: React.FC<Props> = ({ setIsSearchOpen }) => {
-  const [search, setSearch] = useState<string>("");
-
-  const navigate = useNavigate();
-
-  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setSearch(value);
-  };
-
   return (
     <ClickOutsideWrapper
       onClose={() => {
@@ -28,11 +19,7 @@ const Search: React.FC<Props> = ({ setIsSearchOpen }) => {
         className="grid grid-rows-1 grid-cols-[1fr_auto] items-center justify-center px-10 md:px-52 mt-32"
         onSubmit={() => setIsSearchOpen(false)}
       >
-        <input
-          name="search"
-          className="bg-white h-full p-2"
-          onChange={(e) => handleValueChange(e)}
-        ></input>
+        <input name="search" className="bg-white h-full p-2"></input>
         <Button variant="primary-btn" type="submit">
           Search
         </Button>
